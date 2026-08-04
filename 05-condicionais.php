e19a558c6557b2bf1a9d2cfba0d826d5ca2dd1e3
rochamolasluizfernando-rgb <rochamolasluizfernando@gmail.com>
2026-08-04 09:06:10 -0300

    Exemplo de condicional composta

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -4,9 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Condicionais</title>
+    <style>
+        .comprar{ color: red; }
+        .normal{ color: blue; }
+    </style>
 </head>
 <body>
     <h1>Trabalhando com estruturas condicionais</h1>
     <hr>
+
+    <!-- Lembrete: ao usar condicionais, muitas vezes 
+    também usaremos operadores relacionais:
+    <, <=, >, >=, ==, !=, ===, !== -->
+
+    <h2>Condicionais SIMPLES: <code>if</code></h2>
+
+<?php 
+    $numero = 50;
+
+    // Estrutura tradicional (comando, parênteses, chaves)
+    if($numero > 10){
+        echo "<p>$numero é maior que 10.</p>";
+    }
+
+    // Estrutura abreviada (sem chaves)
+    if($numero > 10) echo "<p>$numero é maior que 10.</p>";
+
+    // Estrutura alternativa (sem chaves, com : e endif)
+    if($numero > 10):
+        echo "<p>$numero é maior que 10.</p>";
+    endif;
+?>
+    <hr>
+
+    <h2>Condicional COMPOSTA: <code>if/else</code></h2>
+<?php 
+$produto = "Ultrabook";
+$qtdEmEstoque = 3; //o que temos a disposição
+$qtdCritica = 5; // mínimo necessário no estoque
+?>
+    <h3><?= $produto ?></h3>
+    <p><b>Quantidade em estoque: </b><?= $qtdEmEstoque ?></p>
+
+<?php 
+    if ($qtdEmEstoque < $qtdCritica) {
+        echo "<p>É necessário comprar/repor</p>";
+    } else {
+        echo "<p>Estoque normal.</p>";
+    } 
+?>
+
+
 </body>
 </html>
