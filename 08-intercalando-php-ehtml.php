60adad5a7383d2c0048f27fc2c0eaab49e39a67f
rochamolasluizfernando-rgb <rochamolasluizfernando@gmail.com>
2026-08-05 08:58:50 -0300

    Revisa mistura HTML e PHP

---
+++ 08-intercalando-php-ehtml.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Revisando PHP intercalando com HTML</title>
+</head>
+<body>
+    <h1>Revisando PHP intercalando com HTML</h1>
+    <hr>
+<?php 
+$aluno = "Fulano";
+const ESCOLA = "Senac Penha";
+
+echo "<p>O aluno se chama $aluno</p>";
+echo "<p>Ele estuda na escola ".ESCOLA."</p>";
+?>
+    <h2>Usando PHP intercalando com HTML</h2>
+    <p><i>(usar o PHP onde precisa)</i></p>
+    <p>O aluno se chama <?= $aluno ?></p>
+    <p>Ele estuda na escola <?= ESCOLA ?></p>
+
+    <hr>
+
+    <h2>Usando PHP intercalando comandos com HTML</h2>
+
+<?php $idade = 25; ?> <!-- PHP onde preciso -->
+
+    <h3>Resultado:</h3>
+<?php 
+if ($idade >= 18) {
+    echo "<p><b>$aluno</b> é maior de idade</p>";
+} else {
+    echo "<p><i>$aluno</i> é menor de idade</p>";
+}
+?>
+    <h3>Resultado (usando PHP só onde é necessário):</h3>
+
+</body>
+</html>
