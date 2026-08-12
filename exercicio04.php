5ef14ed93aa173049d19e309c567add281b8e1fb
rochamolasluizfernando-rgb <rochamolasluizfernando@gmail.com>
2026-08-12 09:21:32 -0300

    resolvendo o exercicio

--- exercicio04.php
+++ exercicio04.php
@@ -10,37 +10,37 @@
     <h1>Linguagens e Tecnologias Web</h1>
     <hr>
 
-<?php
-$linguagens = [
-    [
-        "id" => 1,
-        "nome" => "HTML",
-        "descricao" => "Estruturação de páginas web"
-    ],
-    [
-        "id" => 2,
-        "nome" => "CSS",
-        "descricao" => "Estilos"
-    ],
-    [
-        "id" => 3,
-        "nome" => "JavaScript",
-        "descricao" => "Comportamentos"
-    ],
-    [
-        "id" => 4, 
-        "nome" => "PHP",
-        "descricao" => "Back-End" 
-    ],
-    [
-        "id" => 5,
-        "nome" => "SQL",
-        "descricao" => "Manipulação de Dados"
-    ]
-];
-?>
-    <table class="table table-striped table-bordered table-hover">
-        <thead class="table-dark">
+    <?php
+    $linguagens = [
+        [
+            "id" => 1,
+            "nome" => "HTML",
+            "descricao" => "Estruturação de páginas web"
+        ],
+        [
+            "id" => 2,
+            "nome" => "CSS",
+            "descricao" => "Estilos"
+        ],
+        [
+            "id" => 3,
+            "nome" => "JavaScript",
+            "descricao" => "Comportamentos"
+        ],
+        [
+            "id" => 4,
+            "nome" => "PHP",
+            "descricao" => "Back-End"
+        ],
+        [
+            "id" => 5,
+            "nome" => "SQL",
+            "descricao" => "Manipulação de Dados"
+        ]
+    ];
+    ?>
+    <table border="1">
+        <thead>
             <tr>
                 <th>ID</th>
                 <th>Linguagem</th>
@@ -50,9 +50,9 @@ $linguagens = [
         <tbody>
             <?php foreach ($linguagens as $linguagem): ?>
                 <tr>
-                    <td><?php echo $linguagem["id"]; ?></td>
-                    <td><?php echo $linguagem["nome"]; ?></td>
-                    <td><?php echo $linguagem["descricao"]; ?></td>
+                    <td><?= $linguagem["id"]; ?></td>
+                    <td><?= $linguagem["nome"]; ?></td>
+                    <td><?= $linguagem["descricao"]; ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
