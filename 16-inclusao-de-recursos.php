20bc887e0c3d55f978105d8b84d058b7f4f8ca70
rochamolasluizfernando-rgb <rochamolasluizfernando@gmail.com>
2026-08-19 10:57:37 -0300

    Exemplo de carregamento de conteúdo parcial/fragmento

--- 16-inclusao-de-recursos.php
+++ 16-inclusao-de-recursos.php
@@ -21,7 +21,20 @@
     <p>Estamos estudando no <?= ESCOLA ?> fazendo o curso <?= $curso ?>.</p>
     <p>Para fazer este curso o aluno deve ser maior idade.</p>
     <p>Como você <?= ALUNO ?> tem 20 anos, você é <?= verificarIdade(20) ?></p>
-    </div>
+    
+    <h2>Exemplo de carregamento de conteúdo parcial/fragmento</h2>
+    <section>
+        <h3>Título qualquer</h3>
+        <?php include "textos.php"; ?>
+
+        <ul>
+            <?php foreach($tecnologias as $tecnologia): ?>
+                <li><?= $tecnologia ?></li>
+            <?php endforeach ?>
+        </ul>
+    </section>
+
+</div>
     
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
---
+++ textos.php
@@ -0,0 +1,5 @@
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quisquam quia quam facere? Ducimus, distinctio illum debitis illo iure omnis atque architecto odit sapiente! Praesentium, magnam. Quibusdam debitis vero ipsam?
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores deleniti ex et recusandae a perferendis, dignissimos, quis perspiciatis possimus iure, laudantium corrupti commodi aperiam? Exercitationem iure nobis repellendus reiciendis.</p>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, atque obcaecati tenetur quam nesciunt quas qui id, dolores perferendis molestias tempora consequatur soluta. Placeat ipsum, incidunt odio voluptates blanditiis quod.</p>
